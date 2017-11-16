045fbfd148c92fe2f3f6845ad88fa39070e78a79
ajinkyawadekar <ajinkyawadekar21290@gmail.com>
2017-11-16 21:58:48 +0000

    sphinx_py3doc_enhanced_theme trial

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,6 +32,7 @@
 # ones.
 extensions = ['sphinx.ext.autodoc']
 
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ytemplates']
 
@@ -82,10 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_py3doc_enhanced_theme
-html_theme = "sphinx_py3doc_enhanced_theme"
-html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_py3doc_enhanced_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
